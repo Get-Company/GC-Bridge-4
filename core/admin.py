deef896b48265cfb7cdd4e1b615569f016dc3fef
GetCompany <contact@get-company.com>
2026-02-10 13:08:53 +0100

    Add Price admin and Unfold base inlines

--- core/admin.py
+++ core/admin.py
@@ -4,12 +4,24 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group, User
 
 from unfold.admin import ModelAdmin as UnfoldModelAdmin
+from unfold.admin import StackedInline as UnfoldStackedInline
+from unfold.admin import TabularInline as UnfoldTabularInline
 from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
 
 
 class BaseAdmin(UnfoldModelAdmin):
     readonly_fields = ("created_at", "updated_at")
     ordering = ("-created_at",)
+
+
+class BaseTabularInline(UnfoldTabularInline):
+    readonly_fields = ("created_at", "updated_at")
+    extra = 0
+
+
+class BaseStackedInline(UnfoldStackedInline):
+    readonly_fields = ("created_at", "updated_at")
+    extra = 0
 
 
 admin.site.unregister(User)
--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 
-from core.admin import BaseAdmin
-from .models import Product
+from core.admin import BaseAdmin, BaseTabularInline
+from .models import Price, Product
+
+
+class PriceInline(BaseTabularInline):
+    model = Price
+    fields = (
+        "price",
+        "rebate_quantity",
+        "rebate_price",
+        "special_price",
+        "special_start_date",
+        "special_end_date",
+        "created_at",
+        "updated_at",
+    )
 
 
 @admin.register(Product)
@@ -9,3 +23,11 @@ class ProductAdmin(BaseAdmin):
     list_display = ("sku", "name", "is_active", "created_at")
     search_fields = ("sku", "name")
     list_filter = ("is_active",)
+    inlines = (PriceInline,)
+
+
+@admin.register(Price)
+class PriceAdmin(BaseAdmin):
+    list_display = ("product", "price", "special_price", "rebate_price", "created_at")
+    search_fields = ("product__erp_nr", "product__name")
+    list_filter = ("created_at",)
