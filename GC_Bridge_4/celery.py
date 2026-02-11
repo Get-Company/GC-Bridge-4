cd11b6f1fea6d6361397be5343377efbe9a4d9ef
GetCompany <contact@get-company.com>
2026-02-11 12:18:07 +0100

    Remove Celery/Redis integration

--- GC_Bridge_4/celery.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from celery import Celery
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GC_Bridge_4.settings")
-
-app = Celery("GC_Bridge_4")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
--- core/admin.py
+++ core/admin.py
@@ -7,17 +7,6 @@ from unfold.admin import ModelAdmin as UnfoldModelAdmin
 from unfold.admin import StackedInline as UnfoldStackedInline
 from unfold.admin import TabularInline as UnfoldTabularInline
 from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
-from unfold.widgets import UnfoldAdminSelectWidget, UnfoldAdminTextInputWidget
-
-from django_celery_beat import admin as celery_admin
-from django_celery_beat.admin import PeriodicTaskForm, TaskSelectWidget
-from django_celery_beat.models import (
-    ClockedSchedule,
-    CrontabSchedule,
-    IntervalSchedule,
-    PeriodicTask,
-    SolarSchedule,
-)
 
 
 class BaseAdmin(UnfoldModelAdmin):
@@ -48,53 +37,4 @@ class UserAdmin(BaseUserAdmin, UnfoldModelAdmin):
 
 @admin.register(Group)
 class GroupAdmin(BaseGroupAdmin, UnfoldModelAdmin):
-    pass
-
-
-admin.site.unregister(IntervalSchedule)
-admin.site.unregister(PeriodicTask)
-admin.site.unregister(CrontabSchedule)
-admin.site.unregister(SolarSchedule)
-admin.site.unregister(ClockedSchedule)
-
-BasePeriodicTaskAdmin = getattr(celery_admin, "PeriodicTaskAdmin", admin.ModelAdmin)
-BaseIntervalScheduleAdmin = getattr(celery_admin, "IntervalScheduleAdmin", admin.ModelAdmin)
-BaseCrontabScheduleAdmin = getattr(celery_admin, "CrontabScheduleAdmin", admin.ModelAdmin)
-BaseSolarScheduleAdmin = getattr(celery_admin, "SolarScheduleAdmin", admin.ModelAdmin)
-BaseClockedScheduleAdmin = getattr(celery_admin, "ClockedScheduleAdmin", admin.ModelAdmin)
-
-
-class UnfoldTaskSelectWidget(UnfoldAdminSelectWidget, TaskSelectWidget):
-    pass
-
-
-class UnfoldPeriodicTaskForm(PeriodicTaskForm):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.fields["task"].widget = UnfoldAdminTextInputWidget()
-        self.fields["regtask"].widget = UnfoldTaskSelectWidget()
-
-
-@admin.register(PeriodicTask)
-class PeriodicTaskAdmin(BasePeriodicTaskAdmin):
-    form = UnfoldPeriodicTaskForm
-
-
-@admin.register(IntervalSchedule)
-class IntervalScheduleAdmin(BaseIntervalScheduleAdmin):
-    pass
-
-
-@admin.register(CrontabSchedule)
-class CrontabScheduleAdmin(BaseCrontabScheduleAdmin):
-    pass
-
-
-@admin.register(SolarSchedule)
-class SolarScheduleAdmin(BaseSolarScheduleAdmin):
-    pass
-
-
-@admin.register(ClockedSchedule)
-class ClockedScheduleAdmin(BaseClockedScheduleAdmin):
     pass
