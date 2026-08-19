232f1c805f3e2b792443b698dab1cc4cc6ca32e9
GetCompany <contact@get-company.com>
2026-08-19 17:59:55 +0200

    Isolate RuleTrigger test from seeded trigger data

--- microtech/test_rule_engine_models.py
+++ microtech/test_rule_engine_models.py
@@ -6,15 +6,15 @@ from microtech.models import RuleTrigger
 class RuleTriggerModelTest(TestCase):
     def test_for_task_returns_active_triggers_for_task_name(self):
         RuleTrigger.objects.create(
-            code="order_create", label="Bestellung anlegen",
-            task_name="orders.microtech_order_upsert", context_root="orders.Order",
+            code="test_trigger_active", label="Aktiv",
+            task_name="test.dummy_task", context_root="orders.Order",
         )
         RuleTrigger.objects.create(
-            code="inactive", label="Aus", task_name="orders.microtech_order_upsert",
+            code="test_trigger_inactive", label="Aus", task_name="test.dummy_task",
             context_root="orders.Order", is_active=False,
         )
-        result = list(RuleTrigger.for_task("orders.microtech_order_upsert"))
-        self.assertEqual([t.code for t in result], ["order_create"])
+        result = list(RuleTrigger.for_task("test.dummy_task"))
+        self.assertEqual([t.code for t in result], ["test_trigger_active"])
 
 
 class OrderRuleEngineFieldsTest(TestCase):
